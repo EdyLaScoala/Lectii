739796b5fddd601967532c6c87781a884e97a101
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-21 16:25:22 +0000

    Created homework folder

---
+++ Matrices/Exercises/Homework/Ex1/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int v[n][n];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin >> v[i][j];
+        }
+    }
+
+
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
