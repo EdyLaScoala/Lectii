e1cbc5fa489fd9e1c7f5a406e5dcb3287751dccf
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-21 09:56:19 +0000

    added homework to main.cpp

--- Matrices/Exercises/Ex1/main.cpp
+++ Matrices/Exercises/Ex1/main.cpp
@@ -27,3 +27,9 @@ int main()
     }
     return 0;
 }
+
+// TEMA: matrice patrata cu n elemente intregi, n maxim 100. Sa se determine:
+
+// Suma elementelor de pe diagonala principala
+// Produsul elementelor de pe diagonala secundara
+// Suma elementelor de deasupra intersectiei diagonalelor (principala/secundara)
