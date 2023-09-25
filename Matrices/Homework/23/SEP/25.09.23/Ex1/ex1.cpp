64545d02f4ef44e1949b4c0f5a51124c8eb7da2d
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-25 16:35:06 +0000

    Ex 1

---
+++ Matrices/Homework/23/SEP/25.09.23/Ex1/ex1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("bac.in");
+
+int main()
+{
+    int m=1000, n=1000;
+    fin >> m >> n;
+    int v[m][n], max[m];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            fin >> v[i][j];
+        }
+    }
+
+
+
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            
+        }
+        cout << endl;
+    }
+    return 0;
+}
