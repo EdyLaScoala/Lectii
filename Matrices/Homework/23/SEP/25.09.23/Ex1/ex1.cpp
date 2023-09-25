c5af740e045953b671eca33d990f829ec8429b47
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-25 16:52:03 +0000

    Ex. 1

--- Matrices/Homework/23/SEP/25.09.23/Ex1/ex1.cpp
+++ Matrices/Homework/23/SEP/25.09.23/Ex1/ex1.cpp
@@ -9,7 +9,7 @@ int main()
 {
     int m=1000, n=1000;
     fin >> m >> n;
-    int v[m][n], max[m];
+    int v[m][n];
     for(int i=0; i<m; i++)
     {
         for(int j=0; j<n; j++)
@@ -18,13 +18,11 @@ int main()
         }
     }
 
-
-
     for(int i=0; i<m; i++)
     {
         for(int j=0; j<n; j++)
         {
-            
+            cout << v[m-i-1][j] << " ";
         }
         cout << endl;
     }
