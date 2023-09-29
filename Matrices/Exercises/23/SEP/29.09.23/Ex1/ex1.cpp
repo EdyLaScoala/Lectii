4e6529e48d06fe2dfa1e91009349b9056563dde0
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-29 08:44:36 +0000

    Exercise

--- Matrices/Exercises/23/SEP/29.09.23/Ex1/ex1.cpp
+++ Matrices/Exercises/23/SEP/29.09.23/Ex1/ex1.cpp
@@ -3,79 +3,38 @@
 
 using namespace std;
 
-void matricePara()
-{
-    ifstream fin("matrice.in");
-    cout << "Par" << endl;
-    int n=100;
-    fin >> n;
-    int v[n][n];
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            fin >> v[i][j];
-        }
-    }
-
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            cout << v[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void matriceImpara()
-{
-    ifstream fin("matrice.in");
-    cout << "Impar" << endl;
-    int n;
-    fin >> n;
-    int v[n][n];
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            fin >> v[i][j];
-        }
-    }
-
-    for(int k=0; k<=(n/2)+1; k++)
-    {
-        for(int i=((n/2))-k; i<=k; i++)
-        {
-            for(int j=((n/2))-k; j<=k; j++)
-            {
-                if((i == -1 || i == k) || (j == -1 || j == k))
-                {
-                    cout << v[i+1][j+1] << " ";
-                }
-                else
-                {
-                    cout << "  ";
-                }
-            }
-            cout << endl;
-        }
-        cout << endl << endl;
-    }
-}
-
 int main()
 {
     ifstream fin("matrice.in");
     int n;
     fin >> n;
-    fin.close();
-    if(n%2==0)
+    int v[n][n];
+    for(int i=0; i<n; i++)
     {
-        matricePara();
+        for(int j=0; j<n; j++)
+        {
+            fin >> v[i][j];
+        }
     }
-    else
+
+    for(int k=0; k<n/2; k++)
     {
-        matriceImpara();
+        for(int j=k; j<n-k-1; j++)
+        {
+            cout << v[k][j] << " ";
+        }
+        for(int i=k; i<n-k-1; i++)
+        {
+            cout << v[i][n-1-k] << " ";
+        }
+        for(int j=n-1-k; j>k-1; j--)
+        {
+            cout << v[n-1-k][j] << " ";
+        }   
+        for(int i=n-1-k; i>k-1; i--)
+        {
+            cout << v[i][k] << " ";
+        }
+        cout << endl;
     }
 }
