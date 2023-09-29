609b3718c0c4b00f561c2e0b956c9bf9083c6232
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-29 08:46:54 +0000

    Homework

---
+++ Matrices/Homework/23/SEP/29.09.23/Ex1/ex1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream fin("matrice.in");
+    int n;
+    fin >> n;
+    int v[n][n];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            fin >> v[i][j];
+        }
+    }
+
+    for(int k=0; k<=(n/2)+1; k++)
+    {
+        for(int i=((n/2))-k; i<=k; i++)
+        {
+            for(int j=((n/2))-k; j<=k; j++)
+            {  
+                cout << v[i+1][j+1] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl << endl;
+    }
+}
