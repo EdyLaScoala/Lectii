0cb9d441fcf771d1212f0ffeee33a359e36b67a1
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-09-29 08:49:35 +0000

    Homework

--- Matrices/Homework/23/SEP/29.09.23/Ex1/ex1.cpp
+++ Matrices/Homework/23/SEP/29.09.23/Ex1/ex1.cpp
@@ -19,10 +19,11 @@ int main()
 
     for(int k=0; k<=(n/2)+1; k++)
     {
+        //cout << k << " " << ((n/2)+1)-k << endl;
         for(int i=((n/2))-k; i<=k; i++)
         {
             for(int j=((n/2))-k; j<=k; j++)
-            {  
+            {
                 cout << v[i+1][j+1] << " ";
             }
             cout << endl;
