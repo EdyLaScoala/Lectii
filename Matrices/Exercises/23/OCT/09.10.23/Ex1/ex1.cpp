b3153279838d4692bbaba53d3d5ad21922623ed1
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-10-09 07:53:07 +0000

    Exercise (unfinished)

--- Matrices/Exercises/23/OCT/09.10.23/Ex1/ex1.cpp
+++ Matrices/Exercises/23/OCT/09.10.23/Ex1/ex1.cpp
@@ -5,28 +5,149 @@ using namespace std;
 
 ifstream fin("data.txt");
 
-int main()
+int v[100][100], n;
+
+void a()
 {
-    int m, n, aux;
-    fin >> m >> n;
-    int v[m][n];
-    for(int i=0; i<m; i++)
+    for(int i=0; i<n; i++)
     {
-        for(int j=0; j<n; j++)
+        for (int j = 0; j < n; j++)
         {
-            fin >> v[i][j];
-        }
-    }
-
-    
-
-    for(int i=0; i<m; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            cout << v[i][j] << " ";
+            cout << i+1 << " ";
         }
         cout << endl;
     }
-    return 0;
+    cout << endl;
+}
+
+void b()
+{
+    for(int i=0; i<n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if((i==n-1 && j==0) || (i==0 && j==n-1))
+            {
+                cout << 1 << " ";
+            }
+            else if (i==0 && i!=j)
+            {
+                cout << 2 << " ";
+            }
+            else if(i==n-1 && i!=j)
+            {
+                cout << 5 << " ";
+            }
+            else if(j==0 && i!=j)
+            {
+                cout << 3 << " ";
+            }
+            else if(j==n-1 && i!=j)
+            {
+                cout << 4 << " ";
+            }
+            else
+            {
+                cout << 1 << " ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void c()
+{
+    for(int i=0; i<n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           cout << i+j+1 << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void d()
+{
+    for(int i=0; i<n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           cout << min(i, j)+1 << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void e()
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n0; j++)
+        {
+            if(i%2==0)
+        {
+            for(int j=n*i; j<n*(i+1); j++)
+            {
+                cout << j+1 << " ";
+            }
+            cout << endl;
+                
+        }
+        else
+        {
+            for(int j=(n*(i+2)); j>=n*(i+1)+1; j--)
+            {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+        }
+        cout << endl;
+             
+        
+    }
+    cout << endl;
+}
+
+int main()
+{
+    fin >> n;
+    char mode;
+    while(true)
+    {
+        cout << "Please input a mode from a to f: ";
+        cin >> mode;
+        switch(mode)
+        {
+        case 'a':
+            a();
+            break;
+        
+        case 'b':
+            b();
+            break;
+        
+        case 'c':
+            c();
+            break;
+
+        case 'd':
+            d();
+            break;
+
+        case 'e':
+            e();
+            break;
+        
+        case 's':
+            return 0;
+
+        default:
+            cout << "Invalid mode." << endl;
+        }
+    }
 }
