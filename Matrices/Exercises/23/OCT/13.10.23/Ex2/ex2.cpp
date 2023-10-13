b2542175e614fad702b36fa0c1fd6bd443dd4690
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-10-13 08:33:49 +0000

    Exercise

--- Matrices/Exercises/23/OCT/13.10.23/Ex2/ex2.cpp
+++ Matrices/Exercises/23/OCT/13.10.23/Ex2/ex2.cpp
@@ -19,14 +19,4 @@ int main()
     }
 
     
-
-    for(int i=0; i<m; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            cout << v[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
 }
