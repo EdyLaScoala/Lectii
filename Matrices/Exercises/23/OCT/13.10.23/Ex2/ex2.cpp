2248700622d17827326765c5d38aa644058e8551
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-10-13 08:44:56 +0000

    Exercise

--- Matrices/Exercises/23/OCT/13.10.23/Ex2/ex2.cpp
+++ Matrices/Exercises/23/OCT/13.10.23/Ex2/ex2.cpp
@@ -7,16 +7,39 @@ ifstream fin("data.txt");
 
 int main()
 {
-    int m, n, aux;
-    fin >> m >> n;
-    int v[m][n];
-    for(int i=0; i<m; i++)
+    int n;
+    fin >> n;
+    int v[n][n];
+    for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
         {
             fin >> v[i][j];
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+             if(v[i][i]>v[i][j])
+             {
+                for(int k=0; j<n; k++)
+                {
+                    swap(v[i][k], v[j][k]);
+                }
+             }
         }
     }
 
-    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
