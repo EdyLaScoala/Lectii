b22db1c68eed18bd8f3aad6f16d314b260c61456
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-10-20 08:34:26 +0000

    Exercise

---
+++ Matrices/Exercises/23/OCT/20.10.23/Ex3/ex3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream fin("data.txt");
+
+int main()
+{
+    int m, n, aux;
+    fin >> m >> n;
+    int v[m][n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            fin >> v[i][j];
+        }
+    }
+
+    int i=0, j=0, sum=0;
+
+    while(i<=n && j<=n)
+    {
+        if(v[i+1][j] > v[i+1][j+1])
+        {
+            sum += v[i++][j];
+        }
+        else
+        {
+            sum += v[i++][j++];
+        }
+    }
+    cout << sum;
+}
