61eb1ff9626e17828373434c5b28d789862ca61a
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2023-12-20 14:13:52 +0000

    eliza...?

---
+++ Tests/Help for Poor Souls/eliza da test si urmeaza sa aiba crize existentiale/eliza8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n=1;
+    cin >> n;
+    int min = n;
+    while(n!=0)
+    {
+        cin >> n;
+        if(n<min && n!=0) min=n;
+    }
+    cout << min;
+}
---
+++ Tests/Help for Poor Souls/eliza da test si urmeaza sa aiba crize existentiale/eliza9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n=1;
+    cin >> n;
+    int max = n;
+    while(n!=0)
+    {
+        cin >> n;
+        if(n>max && n!=0) max=n;
+    }
+    cout << max;
+}
