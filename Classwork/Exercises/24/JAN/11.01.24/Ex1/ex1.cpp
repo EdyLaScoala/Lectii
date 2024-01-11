875355e4e5842741577faef92e1bc3d62078ed61
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-01-11 09:24:07 +0000

    Exercise

---
+++ Classwork/Exercises/24/JAN/11.01.24/Ex1/ex1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+ifstream fin("data.txt");
+
+int main()
+{
+    bool inWord = false, leftWord = false;
+    short words = 0;
+    char s[255];
+    cin.getline(s, 255);
+    for(int i=0; i<strlen(s); i++)
+    {
+        if(s[i]!=' ') inWord=true, leftWord=false; else leftWord = true, inWord=false;
+        if(!inWord) words++;
+    }
+    cout << words;
+    return 0;
+}
