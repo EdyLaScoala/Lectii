b7531eb4e7abfc0213d932ce5789131b97616c01
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-01-25 09:16:40 +0000

    Exercise

--- Classwork/Exercises/24/JAN/22.01.24/Ex2/ex2.cpp
+++ Classwork/Exercises/24/JAN/22.01.24/Ex2/ex2.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <fstream>
 #include <cstring>
 
 using namespace std;
 
+ofstream fout("data.txt");
+ifstream fin("data.txt");
+
 int main()
 {
-    char s[255]={" "}, s2[255] = {" "};
+    char s[255]={" "}, s2[255] = {" "}, temp[255];
     cin.getline(s, 255);
     char* p;
     p = strtok(s, " ");
@@ -14,13 +18,19 @@ int main()
     while(p!=0)
     {
         strcpy(s2, p);
-        //cout << p << endl;
-        strncpy(p+strlen(p)/2, p+strlen(p)/2+1, strlen(p)/2);
-        cout << p << " ";
+        if(strlen(p)>2 and strlen(p)%2!=0)
+        { 
+            strncpy(p+strlen(p)/2, p+strlen(p)/2+1, strlen(p)/2);
+            strncpy(p+strlen(p)-1, " ", 1);
+        }
+        fout << p << " ";
         strcat(s2, p);
         p = strtok(NULL, " ");
     }
-    cout << s2;
+    fout.close();
+    fin.getline(s, 255);
+    fin.close();
+    cout << s;
 }
 
 //      pictura prin aceea arata o pace profunda
