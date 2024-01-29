c1138ccecc20598c52068d4650ed6c55fe25e794
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-01-29 08:17:39 +0000

    Exercise

--- Classwork/Exercises/24/JAN/26.01.24/Ex1/ex1.cpp
+++ Classwork/Exercises/24/JAN/26.01.24/Ex1/ex1.cpp
@@ -9,14 +9,16 @@ ifstream fin("data.txt");
 int main()
 {
     int m=0, n=0;
-    cin >> m >> n;
+    fin >> m >> n;
     int v[m][n];
+    bool show[m][n];
     int maxPos[2]={0, 0},  minPos[2]={0, 0};
     for(int i=0; i<m; i++)
     {
         for(int j=0; j<n; j++)
         {
             fin >> v[i][j];
+            show[i][j] = true;
         }
     }
     int min = v[minPos[0]][minPos[1]] , max = v[maxPos[0]][maxPos[1]];
@@ -28,4 +30,23 @@ int main()
             if(v[i][j]>max) max = v[i][j], maxPos[0] = i, maxPos[1]=j;
         }
     }
+    cout << "min: " << min << " - " << minPos[0] << ", " << minPos[1] << endl;
+    cout << "max: " << max << " - " << maxPos[0] << ", " << maxPos[1] << endl;
+
+    for(int i=0; i<m; i++) show[minPos[0]][i] = false;
+    for(int i=0; i<n; i++) show[i][minPos[1]] = false;
+
+    for(int i=0; i<m; i++) show[maxPos[0]][i] = false;
+    for(int i=0; i<n; i++) show[i][maxPos[1]] = false;
+
+    m-=2;
+    n-=2;
+
+    cout << m << " " << n << endl;
+
+    for(int i=0; i<n; i++)
+    {
+        
+    }
+
 }
