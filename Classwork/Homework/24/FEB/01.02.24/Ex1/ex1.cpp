5785b094408c124c278b77b0f7a913ba8a65c7d7
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-02-01 09:48:44 +0000

    Homework

--- Classwork/Homework/24/FEB/01.02.24/Ex1/ex1.cpp
+++ Classwork/Homework/24/FEB/01.02.24/Ex1/ex1.cpp
@@ -1,14 +1,75 @@
 #include <iostream>
 #include <cstring>
+#include <math.h>
 
 using namespace std;
 
-int main()
+void space()
 {
-    char s[] = {" "};
-    for(int i=0; i<strlen(s); i++)
+    cout << endl << endl << endl;
+}
+
+void descompunere()
+{
+    int n = 0;
+    cout << "Va rugam introduceti un numar: ";
+    cin >> n;
+    int n2 = 0;
+    while(n>0)
     {
-        
+        n2 *= 10;
+        n2 += n%10;
+        n/=10;
     }
-    return 0;
+    while(n2>0)
+    {
+        cout << n2%10 << " ";
+        n2/=10;
+    }
+    space();
+}
+
+void invers()
+{
+    int n = 0;
+    cout << "Va rugam introduceti un numar: ";
+    cin >> n;
+    int n2 = 0;
+    while(n!=0)
+    {
+        n2 *= 10;
+        n2 += n%10;
+        n/=10;
+    }
+    cout << n2;
+    space();
+}
+
+int main(){
+    unsigned short cmd = 0;
+    
+    while(true)
+    {
+        cout << "Please enter a command ID: ";
+        cin >> cmd;
+        switch (cmd)
+        {
+        case 0:
+            space();
+            return 0;
+            break;
+
+        case 1:
+            descompunere();
+            break;
+
+        case 2:
+            invers();
+            break;
+        
+        default:
+            cout << "Unknown commmand ID. Please try again.\n";
+        }
+    }
+    
 }
