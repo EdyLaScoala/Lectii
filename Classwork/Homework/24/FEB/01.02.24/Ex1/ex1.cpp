024e3149163df12eccf676e36b3b59890d0096f2
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-02-01 11:04:04 +0000

    Work In Progres

--- Classwork/Homework/24/FEB/01.02.24/Ex1/ex1.cpp
+++ Classwork/Homework/24/FEB/01.02.24/Ex1/ex1.cpp
@@ -1,12 +1,15 @@
+//algoritm pentru trecerea unei valori din orice baza in baza 10, trecerea din orice abza in baza 2, compunerea unei valori dintr-o sucesiune de max. 9 cifre date - valoarea incepe cu cifra unitatilor, 
+
 #include <iostream>
 #include <cstring>
 #include <math.h>
+#include <stdlib.h>
 
 using namespace std;
 
 void space()
 {
-    cout << endl << endl << endl;
+    cout << endl << endl;
 }
 
 void descompunere()
@@ -21,6 +24,7 @@ void descompunere()
         n2 += n%10;
         n/=10;
     }
+    
     while(n2>0)
     {
         cout << n2%10 << " ";
@@ -45,6 +49,127 @@ void invers()
     space();
 }
 
+bool palindrom()
+{
+    char s[10];
+    cout << "Va rugam introduceti un numar: ";
+    cin >> s;
+    for(int i=0; i<strlen(s)/2; i++)
+    {
+        if(s[i] != s[strlen(s)-i-1])
+        {
+            space();
+            cout << "Numarul nu este palindrom.";
+            return false;
+        }
+    }
+    cout << "Numarul este palindrom.";
+    space();
+    return true;
+}
+
+void divizori()
+{
+    int n = 0;
+    cout << "Va rugam introduceti un numar: ";
+    cin >> n;
+    for(int i=2; i<n/2+1; i++)
+        if(n%i==0) cout << i << " ";
+    space();
+}
+
+bool prim()
+{
+    int n;
+    cout << "Va rugam introduceti un numar: ";
+    cin >> n;
+    for(int i=2; i<sqrt(n); i++)
+    {
+        if(n%i==0)
+        {
+            cout << "Numarul nu este prim.";
+            space();
+            return false;
+        }
+    }
+    cout << "Numarul este prim.";
+    space();
+    return true;
+}
+
+void factoriPrimi()
+{
+    int n=0, n2=0;
+    cout << "Va rugam introduceti un numar: ";
+    cin >> n;
+    n2=n;
+    for(int i=2; i<sqrt(n2); i++)
+    {
+        while (n%i==0)
+        {
+            cout << i << " ";
+            n/=i;
+        }
+    }
+    space();
+}
+
+void cmmdc()
+{
+    int a, b, r, cmmmc, cmmdc;
+    cout << "Introduceti doua numere, a si b: ";
+    cin >> a >> b;
+    cout << endl;
+    while(b!=0 && a%b!=0){
+        r=a%b;
+        a=b;
+        b=r;
+    }
+    cmmdc = r;
+    cout << cmmdc;
+    space();
+}
+
+void cmmmc()
+{
+    int a, b, r, cmmmc;
+    cout << "Introduceti doua numere, a si b: ";
+    cin >> a >> b;
+    cout << endl;
+    while(b!=0 && a%b!=0){
+        r=a%b;
+        a=b;
+        b=r;
+    }
+    cout << cmmmc;
+    space();
+}
+
+void fromBaseXToBase10()
+{
+    int base, n, n2, nr;
+    cout << "Va rugam sa introduceti baza initiala: ";
+    cin >> base;
+    cout << "Va rugam introduceti numarul de convertit in baza 10: ";
+    cin >> n;
+    while(n!=0)
+    {
+        n2*=10;
+        n2+=n%10;
+        n/=10;
+        nr++;
+    }
+    n=0;
+    int i=0;
+    while(n2!=0)
+    {
+        n += (n2 % 10)^i;
+        n2/=10;
+    }
+    cout << n;
+    space();
+}
+
 int main(){
     unsigned short cmd = 0;
     
@@ -55,7 +180,7 @@ int main(){
         switch (cmd)
         {
         case 0:
-            space();
+            cout << "Stopping... " << endl << endl;
             return 0;
             break;
 
@@ -66,7 +191,37 @@ int main(){
         case 2:
             invers();
             break;
+
+        case 3:
+            palindrom();
+            break;
+
+        case 4:
+            divizori();
+            break;
+
+        case 5:
+            prim();
+            break;
+
+        case 6:
+            factoriPrimi();
+            break;
+
+        case 7:
+            cmmdc();
+            break;
+
+        case 8:
+            cmmmc();
+            break;
+
+        case 9:
+            fromBaseXToBase10();
+            break;
+
         
+
         default:
             cout << "Unknown commmand ID. Please try again.\n";
         }
