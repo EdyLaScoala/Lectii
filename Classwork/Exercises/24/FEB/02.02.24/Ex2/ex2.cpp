519707cc110aadf1709b9b16ba4eafdcf23ddc14
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-02-02 09:35:12 +0000

    Exercise

---
+++ Classwork/Exercises/24/FEB/02.02.24/Ex2/ex2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+bool palindrom(int n)
+{
+    int n2=0, n3=n;
+    while(n!=0)
+    {
+        n2*=10;
+        n2+=n%10;
+        n/=10;
+    }
+    if(n2==n3) return true; else return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(palindrom(n))
+    {
+        cout << n;
+    }
+    else
+    {
+        cout << "Numarul nu este palindrom.";
+    }
+}
+
+// se afiseaza n daca e palindrom si n daca nu
