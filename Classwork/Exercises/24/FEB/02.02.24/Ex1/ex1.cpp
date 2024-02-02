3c383a5a6a5f69adcd4ff82b1ddeb03c51c7985e
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-02-02 08:38:50 +0000

    Exercise

--- Classwork/Exercises/24/FEB/02.02.24/Ex1/ex1.cpp
+++ Classwork/Exercises/24/FEB/02.02.24/Ex1/ex1.cpp
@@ -3,12 +3,23 @@
 
 using namespace std;
 
+int suma(int& n, int& a)
+{
+    int s=0;
+    while(n!=0)
+    {
+        if(n%10==a) s+=n%10, n/=10;
+    }
+    cout << "Valoare x: " << n << endl;
+    return s;
+}
+
 int main()
 {
-    char s[] = {" "};
-    for(int i=0; i<strlen(s); i++)
-    {
-        
-    }
-    return 0;
+    int n, a;
+    cin >> n >> a;
+    cout << suma(n, a) << endl;
+    cout <<"Valoare x din main: " << n;
 }
+
+//par 2 de tip a, sa returneze suma cifrelor impare din valoarea data
