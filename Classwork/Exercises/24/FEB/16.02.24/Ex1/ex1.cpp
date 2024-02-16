ec4ebf01a74c7227dd2c09d6f33ec55bb68db974
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-02-16 08:46:39 +0000

    Exercise

--- Classwork/Exercises/24/FEB/16.02.24/Ex1/ex1.cpp
+++ Classwork/Exercises/24/FEB/16.02.24/Ex1/ex1.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
+int read(int v[], int n)
+{
+    int max=0;
+    for(int i=0; i<n; i++)
+    {
+        cin >> v[i];
+        if(v[i]>max) max=v[i];
+    }
+    return max;
+}
 
+int remove(int v[], int n, int pos)
+{
+    for(int i=pos-1; i<n-1; i++)
+    {
+        v[i] = v[i+1];
+    }
+    n--;
+    return n;
+}
 
 int main()
 {
     int n=0;
     cin >> n;
+
+    int v[n];
+
+    int max = read(v, n);
+
+    for(int i=0; i<n; i++)
+    {
+        if(v[i]==max)
+        {
+            n = remove(v, n, i);
+        }
+        cout << v[i] << " ";
+    }
+
+    return 0;
 }
