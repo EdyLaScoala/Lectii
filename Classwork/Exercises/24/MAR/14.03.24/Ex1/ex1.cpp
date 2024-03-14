9946f4367850e90a9a6c37e555af30c9c36cb323
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-03-14 10:24:15 +0000

    Exercise

--- Classwork/Exercises/24/MAR/14.03.24/Ex1/ex1.cpp
+++ Classwork/Exercises/24/MAR/14.03.24/Ex1/ex1.cpp
@@ -6,12 +6,12 @@ using namespace std;
 ifstream fin("bac.in");
 ofstream fout("bac.out");
 
-void p(int &n)
+void p(int &n, int c)
 {
     int a=0, i=1;
     while(n!=0)
     {
-        if(n%2==0)
+        if(n%10!=c)
         {
             a=a+i*(n%10);
             i*=10;
@@ -26,7 +26,8 @@ int main()
     int n;
     while(fin >> n)
     {
-        p(n);
+        for(int i=1; i<10; i+=2)
+            p(n, i);
         if(n!=0)
             fout << n << endl;
     }
---
+++ Classwork/Exercises/24/MAR/14.03.24/Ex2/ex2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int read(int v[], int n)
+{
+    for(int i=0; i<n; i++)
+        cin >> v[i];
+    return *v;
+}
+
+int main()
+{
+    int n=0;
+    cin >> n;
+
+    int v[n];
+    read(v, n);
+
+    return 0;
+}
