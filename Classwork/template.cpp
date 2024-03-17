cf7611d60c3070c9cb1a65b75b791ac0f9137869
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-03-17 16:04:55 +0000

    Template rework

--- Classwork/template.cpp
+++ Classwork/template.cpp
@@ -3,20 +3,10 @@
 
 using namespace std;
 
-int read(int v[], int n)
-{
-    for(int i=0; i<n; i++)
-        cin >> v[i];
-    return *v;
-}
-
 int main()
 {
     int n=0;
     cin >> n;
-
-    int v[n];
-    read(v, n);
 
     return 0;
 }
