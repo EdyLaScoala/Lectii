4aba609d511ffec04b370e201fb0bfc6d4531450
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-04-08 07:07:01 +0000

    Exercise

---
+++ Classwork/Exercises/24/APR/05.04.24/Ex1/ex1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+float cmmdc(int a, int b)
+{
+    float r;
+    while(a%b!=0)
+    {
+        r=a%b;
+        a=b;
+        b=r;
+    }
+    return r;
+}
+
+int main()
+{
+    struct
+    {
+        float x, y;
+    }a, b;
+
+    cin >> a.x >> a.y >> b.x >> b.y;
+    
+    if(a.x/a.y>b.x/b.y)
+    {
+        //cout << "a: " << float(a.x/a.y);
+        while(cmmdc(a.x, a.y) == int(cmmdc(a.x, a.y)))
+        {
+            a.x/=cmmdc(a.x, a.y);
+            a.y/=cmmdc(a.x, a.y);
+        }
+    }
+    else
+    {
+        while(cmmdc(b.x, b.y) == int(cmmdc(b.x, b.y)))
+        {
+            b.x/=cmmdc(b.x, b.y);
+            b.y/=cmmdc(b.x, b.y);
+            cout << b.x << " " << b.y << endl;
+        }
+    }
+
+}
