09825c651ba90fb85f57e4d2f116831ffc642f5a
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-04-17 08:14:51 +0000

    Exercise

--- Classwork/Exercises/24/APR/08.04.24/Ex1/ex1.cpp
+++ Classwork/Exercises/24/APR/08.04.24/Ex1/ex1.cpp
@@ -1,11 +1,68 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-    int n=0;
-    cin >> n;
+struct Record {
+    int id;
+    string name;
+};
+
+vector<Record> database;
+
+void addRecord(int id, string name) {
+    Record r;
+    r.id = id;
+    r.name = name;
+    database.push_back(r);
+}
+
+void viewRecords() {
+    for (Record r : database) {
+        cout << "ID: " << r.id << ", Name: " << r.name << endl;
+    }
+}
+
+void deleteRecord(int id) {
+    for (int i = 0; i < database.size(); i++) {
+        if (database[i].id == id) {
+            database.erase(database.begin() + i);
+            break;
+        }
+    }
+}
+
+int main() {
+    int choice, id;
+    string name;
+
+    while (true) {
+        cout << endl << endl << "1. Add Record\n2. View Records\n3. Delete Record\n4. Exit\nEnter your choice: ";
+        cin >> choice;
+        system("clear");
+        switch (choice) {
+            case 1:
+                cout << "Enter ID: ";
+                cin >> id;
+                cout << "Enter Name: ";
+                cin >> name;
+                addRecord(id, name);
+                break;
+            case 2:
+                viewRecords();
+                break;
+            case 3:
+                cout << "Enter ID of the record to delete: ";
+                cin >> id;
+                deleteRecord(id);
+                break;
+            case 4:
+                exit(0);
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+    }
 
     return 0;
 }
