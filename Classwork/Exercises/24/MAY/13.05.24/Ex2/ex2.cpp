598c91ba7f604826af8e8512bfda1ce62b43940a
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-05-13 07:47:02 +0000

    Exercise

--- Classwork/Exercises/24/MAY/13.05.24/Ex2/ex2.cpp
+++ Classwork/Exercises/24/MAY/13.05.24/Ex2/ex2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int f(int x, int y)
 {
     if(x>y) return 0;
-    else if(x%2==0) return 1 + f(x+1, y);
+    else if(x%2!=0) return 1 + f(x+1, y);
     else return f(x+1, y);
 }
 
