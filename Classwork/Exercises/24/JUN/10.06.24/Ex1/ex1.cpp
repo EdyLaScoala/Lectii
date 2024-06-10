5a637aefe4536e45fef4d9c2fa54c2ef7fa16df6
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-06-10 07:21:37 +0000

    Exercise

---
+++ Classwork/Exercises/24/JUN/10.06.24/Ex1/ex1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    struct
+    {
+        char producator;
+        int frecventa, pret;
+    } procesor[20];
+
+    int n=0;
+    cin >> n;
+
+    return 0;
+}
