723dfd048f49ad5d0aa367a47f1d062bdaffc1fd
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-09-10 11:41:22 +0000

    Exercise

--- Classwork/Exercises/24/SEP/10.09.24/Ex2/ex2.cpp
+++ Classwork/Exercises/24/SEP/10.09.24/Ex2/ex2.cpp
@@ -18,7 +18,7 @@ int main()
     int a=12558842;
     int v[10]={0};
     for(int i=0; i<10; i++) if(f(a, i) % 2 == 0) v[i] = f(a, i);
-    for(int i=9; i>=0; i--)  while(v[i]/2!=0) cout << i, v[i]--; 
-    for(int i=0; i<=9; i++)  while(v[i]!=0) cout << i, v[i]--;
+    for(int i=9; i>=0; i--) while(v[i]/2!=0) cout << i, v[i]--; 
+    for(int i=0; i<=9; i++) while(v[i]!=0) cout << i, v[i]--;
     return 0;
 }
