3f8705bd296cbf50d7c415c4fc631bf66c597521
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-09-10 11:37:13 +0000

    Exercise

--- Classwork/Exercises/24/SEP/10.09.24/Ex2/ex2.cpp
+++ Classwork/Exercises/24/SEP/10.09.24/Ex2/ex2.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+int f(int a, int b)
+{
+    short ret=0;
+    while(a!=0)
+    {
+        if(a%10==b) ret++; 
+        a/=10;
+    }
+    return ret;
+}
+
 int main()
 {
-    int n=0;
-    cin >> n;
-
+    int a=12558842;
+    int v[10]={0};
+    for(int i=0; i<10; i++) if(f(a, i) % 2 == 0) v[i] = f(a, i);
+    for(int i=9; i>=0; i--)  while(v[i]/2!=0) cout << i, v[i]--; 
+    for(int i=0; i<=9; i++)  while(v[i]!=0) cout << i, v[i]--;
     return 0;
 }
