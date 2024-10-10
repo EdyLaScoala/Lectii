c1100d2813936c4059f523e5e54bdf9a20155cab
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-10-10 10:58:56 +0000

    template update

--- Classwork/template.cpp
+++ Classwork/template.cpp
@@ -6,28 +6,26 @@ int m, n, v[100];
 
 void init(int k)
 {
-    v[k] = 0;
+    v[k] = k==1 ? 0 : v[k-1];
 }
 
 void print()
 {
     for(int i=1; i<=m; i++)
     {
-        cout << v[i];
+        cout << v[i] << " ";
     }
     cout << endl;
 }
 
 bool isValid(int k)
 {
-    for(int i=0; i<k; i++)
-        if(v[i] == v[k]) return false;
     return true;
 }
 
 bool hasSuccesor(int k)
 {
-    if(v[k]<n)
+    if(v[k]<n-m+k)
     {
         v[k]++;
         return true;
@@ -40,7 +38,7 @@ bool isSolution(int k)
     return k==m;
 }
 
-int backtrack(int k)
+void backtrack(int k)
 {
     init(k);
     while(hasSuccesor(k))
@@ -54,4 +52,4 @@ int main()
     cin >> m >> n;
     backtrack(1);
     return 0;
-} 
+}
